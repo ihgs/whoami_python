6dca11be9292026a7b2065c825357f5f1a90012b
ihgs <hgs.dev@gmail.com>
2019-01-13 17:10:02 +0900

    show userinfo

---
+++ front/src/components/UserInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="userinfo">
+    <div v-if="info">
+      <h1>{{ info.userid }}</h1>
+      <b-table striped hover :items="info.systems" caption="Systems" caption-top></b-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'UserInfo',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('/api/v1/userinfo')
+      .then( response => {
+        this.info = response.data.userinfo
+       })
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ front/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://127.0.0.1:5042/',
+        changeOrigin: true
+      }
+    }
+  }
+}
